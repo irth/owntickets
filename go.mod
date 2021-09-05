9f35319f2eb4d4aa3f4f9cc9907371f9eb2b49ad
Wojciech Kwolek <wojciech@kwolek.io>
2021-09-05 14:40:16 +0200

    add templating

--- go.mod
+++ go.mod
@@ -5,10 +5,12 @@ go 1.17
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/flosch/pongo2/v4 v4.0.2 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.2 // indirect
 	github.com/mattn/go-sqlite3 v1.14.5 // indirect
+	github.com/nathan-osman/pongo2-embed-loader v1.0.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
--- internal/owntickets/owntickets.go
+++ internal/owntickets/owntickets.go
@@ -1,20 +1,28 @@
 package owntickets
 
 import (
+	"embed"
 	"fmt"
 	"net/http"
 
+	"github.com/flosch/pongo2/v4"
 	"github.com/gorilla/mux"
 	"github.com/irth/owntickets/internal/models"
+	loader "github.com/nathan-osman/pongo2-embed-loader"
 	log "github.com/sirupsen/logrus"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
+//go:embed templates
+var Templates embed.FS
+
 type OwnTickets struct {
 	Config   Config
 	Database *gorm.DB
 	Router   http.Handler
+
+	TicketFormTemplate *pongo2.Template
 }
 
 func (o *OwnTickets) Run() error {
@@ -30,7 +38,10 @@ func (o *OwnTickets) Run() error {
 	}
 	log.Info("Starting webserver")
 	if err := o.SetupRouter(); err != nil {
-		log.WithError(err).Fatal("Failed to start webserver")
+		log.WithError(err).Fatal("Failed to set up routing")
+	}
+	if err := o.SetupTemplates(); err != nil {
+		log.WithError(err).Fatal("Failed to load templates")
 	}
 	http.ListenAndServe(":2137", o.Router)
 	return nil
@@ -48,9 +59,21 @@ func (o *OwnTickets) SetupDatabase() error {
 
 func (o *OwnTickets) SetupRouter() error {
 	r := mux.NewRouter()
+	r.HandleFunc("/", o.TicketPage)
 	r.HandleFunc("/admin", o.AdminPage)
 	o.Router = r
 	return nil
+}
+
+func (o *OwnTickets) SetupTemplates() (err error) {
+	templateSet := pongo2.NewSet("", &loader.Loader{Content: Templates})
+	o.TicketFormTemplate, err = templateSet.FromFile("templates/ticket_form.html")
+	return
+}
+
+func (o *OwnTickets) TicketPage(w http.ResponseWriter, r *http.Request) {
+	o.TicketFormTemplate.ExecuteWriter(nil, w)
+	// TODO: handle POST
 }
 
 func (o *OwnTickets) AdminPage(w http.ResponseWriter, r *http.Request) {
